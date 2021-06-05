b575e8117aa8ff501ff49fe3d621452daee71785
cgerasimovitch <c.gerasimovitch@gmail.com>
2021-06-05 21:06:04 +0300

    Add initial files

---
+++ 2_3_part_01/2_3_part_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_3_part_01
+//
+//  Created by Admin on 6/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
